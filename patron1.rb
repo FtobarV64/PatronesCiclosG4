8635211c3804dedfd10f01e8fe18ccff319cff89
FtobarV64 <ftobarv@gmail.com>
2022-03-26 09:36:25 -0300

    finished: patron1.rb

---
+++ patron1.rb
@@ -0,0 +1,16 @@
+# Patrón 1
+# ruby patron1.rb 10
+# *.*.*.*.*.
+
+nbr = ARGV[0].to_i
+
+nbr.times do |i|
+    if i.even?
+        print "*"
+    else
+        print "."
+    end
+    
+    # (i.even?)? (print "*") : (print ".")
+end
+
